ecb66cc8bc190a5d3ea7307e90ef1966c04ab2c1
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-09-21 10:55:42 +0000

    bring common data to a single yaml file and merge them during generation; add new host aliases

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for i in 1 2 3; do
-    j2 apic-config.j2 datapower-${i}/data.yaml > datapower-${i}/apic-config.cfg
+    cat datapower-${i}/data.yaml common-data.yaml | j2 --format=yaml apic-config.j2  > datapower-${i}/apic-config.cfg
     j2 keygen.j2 datapower-${i}/data.yaml > datapower-${i}/keygen.cfg
 done
