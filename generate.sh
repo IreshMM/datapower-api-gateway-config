0a8ac1d173c4a7caffed5b3212bb89f1428fdaf3
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-09-30 10:18:16 +0000

    restructure for multi-site configuration (active, passive)

--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+TEMPLATE_DIR=templates
 
 for i in 1 2 3; do
-    cat datapower-${i}/data.yaml common-data.yaml | j2 --format=yaml apic-config.j2  > datapower-${i}/apic-config.cfg
-    j2 keygen.j2 datapower-${i}/data.yaml > datapower-${i}/keygen.cfg
+    for site in active-site passive-site; do
+        SITE_CONFIG_DIR=config/${site}
+        cat ${SITE_CONFIG_DIR}/datapower-${i}/data.yaml ${SITE_CONFIG_DIR}/common-data.yaml \
+            | j2 --format=yaml ${TEMPLATE_DIR}/apic-config.j2  > ${SITE_CONFIG_DIR}/datapower-${i}/apic-config.cfg
+        j2 ${TEMPLATE_DIR}/keygen.j2 ${SITE_CONFIG_DIR}/datapower-${i}/data.yaml > ${SITE_CONFIG_DIR}/datapower-${i}/keygen.cfg
+    done
 done
