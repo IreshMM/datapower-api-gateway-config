48a11c724dea34e27b617b16e204d10664ec2d33
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-10-11 17:24:15 +0000

    disable strict host key checking for execute script

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -4,7 +4,7 @@ set host [ lindex $argv 0 ]
 set command [ lindex $argv 1 ]
 
 set timeout -1
-spawn ssh -t $host telnet localhost 2200
+spawn ssh -t -o "StrictHostKeyChecking=no" $host telnet localhost 2200
 match_max 100000
 expect "*login: "
 send -- "admin\r"
