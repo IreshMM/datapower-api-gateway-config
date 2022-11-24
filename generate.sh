6338481d636d8f3166b4a2092eaba0ac51ae966d
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-11-24 12:18:46 +0000

    reshape to test env single gateway

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 TEMPLATE_DIR=templates
 
-for i in 1 2 3; do
-    for site in active-site passive-site; do
+for i in 1; do
+    for site in test-site; do
         SITE_CONFIG_DIR=config/${site}
         cat ${SITE_CONFIG_DIR}/datapower-${i}/data.yaml ${SITE_CONFIG_DIR}/common-data.yaml \
             | j2 --format=yaml ${TEMPLATE_DIR}/apic-config.j2  > ${SITE_CONFIG_DIR}/datapower-${i}/apic-config.cfg
