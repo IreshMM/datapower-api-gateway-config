94f4e820251641bff783437569504af2b526570f
Iresh Dissanayaka <socialexpz1@gmail.com>
2023-07-23 16:35:13 +0000

    parameterize initialize datapower script

--- scripts/initialize.ps1
+++ scripts/initialize.ps1
@@ -1,15 +1,29 @@
+param(
+    [int]$gateway_num,
+    [string]$site_name
+)
+
 . /scripts/powershell/VMKeystrokes.ps1
 
-$vm = "APIConnect-datapower-1"
+if ($site_name -eq "pr") {
+	$prefix = "172.22.8"
+} elseif ($site_name -eq "dr") {
+	$prefix = "172.23.8"
+} else {
+	Write-Host "Wrong site selection!"
+	Exit;
+}
+
+$vm = "APIConnect-datapower-${gateway_num}-${site_name}"
 $username = "admin"
 $password = "admin"
 $newpassword = "admin123"
-$ipcidr1 = "172.22.8.11/26" 
-$gateway1 = "172.22.8.1"
-$ipcidr2 = "172.22.8.76/26" 
-$gateway2 = "172.22.8.65"
+$ipcidr1 = "${prefix}.$(10 + $gateway_num)/26" 
+$gateway1 = "${prefix}.1"
+$ipcidr2 = "${prefix}.$(75 + $gateway_num)/26" 
+$gateway2 = "${prefix}.65"
 $dnsserver = "161.26.0.7"
-$fqdn = "pr-apic-dpgw1.apic.nsb.local"
+$fqdn = "pr-apic-dpgw${gateway_num}.apic.nsb.local"
 $ip2 = $ipcidr2.Substring(0, $ipcidr2.Length - 3)
 $password_reset_user_password = "nsb@12345"
 
