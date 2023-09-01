b162af301633b4e1f6449555b2d9b9ec88b7f9b1
Iresh Dissanayaka <socialexpz1@gmail.com>
2023-09-01 09:20:45 +0000

    simplify initialize script just to take vm name and ip address further configuration is offloaded to config files

--- scripts/initialize.ps1
+++ scripts/initialize.ps1
@@ -1,33 +1,17 @@
 param(
-    [int]$gateway_num,
-    [string]$site_name
+    [string]$node_name,
+    [string]$ip_address
 )
 
 . /scripts/powershell/VMKeystrokes.ps1
 
-if ($site_name -eq "pr") {
-	$prefix = "172.22.8"
-} elseif ($site_name -eq "dr") {
-	$prefix = "172.23.8"
-} else {
-	Write-Host "Wrong site selection!"
-	Exit;
-}
 
-$vm = "APIConnect-datapower-${gateway_num}-nsb-${site_name}"
-$username = "admin"
-$password = "admin"
-$newpassword = "admin123"
-$ipcidr1 = "${prefix}.$(10 + $gateway_num)/26" 
-$gateway1 = "${prefix}.1"
-$ipcidr2 = "${prefix}.$(75 + $gateway_num)/26" 
-$gateway2 = "${prefix}.65"
-$dnsserver = "161.26.0.7"
-$fqdn = "pr-apic-dpgw${gateway_num}.apic.nsb.local"
-$ip2 = $ipcidr2.Substring(0, $ipcidr2.Length - 3)
-$password_reset_user_password = "nsb@12345"
+$VM = $node_name
+$username="admin"
+$password="admin"
+$newpassword="admin123"
 
-$inputs = $username, $password, "", "y", "y", "n", $newpassword, $newpassword, "y", "y", "y", "y", "n", $ipcidr1, $gateway1, "y", "n", $ipcidr2, $gateway2, "y", "y", "y", $dnsserver, "y", $fqdn, "y", "y", "y", $ip2, "22", "y", $ip2, "9090", "y", "password-reset-user", $password_reset_user_password, $password_reset_user_password, "y", "y", "ondisk", "n", "y", "y", "y"
+$inputs = $username, $password, "", "y", "y", "n", $newpassword, $newpassword, "n", "config", "ethernet eth0", "ip-address ${ip_address}", "exit", "web-mgmt 0.0.0.0 9090", "ssh 0.0.0.0 22", "exit", "exit"
 
 foreach($input in $inputs) {
 	Start-Sleep -Seconds 1
